0463840698b912175e6f49948a26c84775d189e4
Rebecca Hawkenson <rehawkenson@gmail.com>
2020-11-04 18:15:14 -0600

    Next step: need to save the last nodes as actual nodes and not just values tied to the parent (left or right)

--- bst.rb
+++ bst.rb
@@ -1,10 +1,13 @@
 class Node
+  include Comparable
   attr_accessor :left, :right, :data
   def initialize(data)
     @data = data
     @left = nil
     @right = nil 
   end 
+
+  
 end 
 
 
@@ -18,13 +21,25 @@ class Tree
   def build_tree(arr)
     middle = arr.length/2
     root = Node.new(arr[middle])
-
     return root.data if arr.length <= 1
-    puts root.data
-    root.left = build_tree(arr[0..(middle-1)])
+    root.left = build_tree(arr[0..(middle-1)]) 
     root.right = build_tree(arr[(middle+1)...arr.length])
     return root
+
   end 
 
-  
+  def insert(value, current = @root.data)
+    if value < current
+      insert(value, root.left.data)
+    end 
+    
+  end
+
+  def pretty_print(node = @root, prefix = '', is_left = true)
+    pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
+    puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}" 
+    pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left 
+  end
+
+
 end 
